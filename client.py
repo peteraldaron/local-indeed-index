96a2e35e4e973323dd3c6ab98a787e2648c6009b
peteraldaron <peteraldaron@gmail.com>
2016-09-01 23:49:29 -0400

    corrected errors

--- client.py
+++ client.py
@@ -1,37 +1,41 @@
 import indeed, db
 from indeed import IndeedClient
-import os, sys, fs
+import os, sys
 
 
+
+#need to initialize database with client
 class Client:
     _singleton = None;
+    _uastring =  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.80 Safari/537.36"
     def __init__(self, clientKey):
         self.client = IndeedClient(clientKey)
+        self.db = db.DataBase();
+        self.clientKey = clientKey
 
-    def queryAll(title="software", location="", country="fi"):
+    def queryAll(self, title="software", location="", country="fi"):
         params = {
                 'q' : title,
                 'l' : location,
-                'userip' : "8.8.8.8",
                 'co' : country,
-                'useragent' : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.80 Safari/537.36"
+                'userip' : '8.8.8.8',
+                'useragent' : Client._uastring
         }
         results = self.client.search(**params)
-        while results.totalResults > results.end:
-            #store results:
-            #TODO
-            params['start'] = results.end
-            results = self.client.search(**params)
-
-
-
+        #aggregate all results first:
+        aggregated_results = [];
+        while 'totalResults' in results\
+                and results['totalResults'] > results['end']:
+            aggregated_results.extend(results['results']);
+            params['start'] = results['end'];
+            results = self.client.search(**params);
+        #store results:
+        self.db.insertManyIntoCollection(aggregated_results, "indeed."+country);
 
 def getClient():
     if Client._singleton is None:
         with open("key") as fp:
-            key = fp.readline()
+            key = fp.readlines()
+            key = key[0][:-1];
             Client._singleton = Client(key)
     return Client._singleton
-
-
-
---
+++ main.py
@@ -0,0 +1,4 @@
+import client
+
+cl = client.getClient();
+cl.queryAll();
